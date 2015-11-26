b36c87ee49683c238b55b216838aad21103728a9
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-11-26 15:49:09 +0200

    Add marker

--- static/viz.js
+++ static/viz.js
@@ -1,7 +1,13 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map_canvas"), {
-    center: {lat: 32.0833, lng: 34.8000},
-    zoom: 8
-  });
+    map = new google.maps.Map(document.getElementById("map_canvas"), {
+        center: {lat: 32.0833, lng: 34.8000},
+        zoom: 8
+    });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 32.0833, lng: 34.8000},
+        map: map,
+        title: 'תל אביב'
+    });
 }
